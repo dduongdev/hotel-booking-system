472141e2d15a6fe8df586b6f974f69a8c10ca2e7
dduongdev <nguyendongduong4551190010@gmail.com>
2026-05-11 12:35:56 +0700

    Optimize performance for task find best room to assign to booking

--- src/main/java/com/dduongdev/hotel/repository/BookingRepository.java
+++ src/main/java/com/dduongdev/hotel/repository/BookingRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -20,6 +21,7 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
     Page<Booking> findByUserId(Integer userId, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"room"})
     Optional<Booking> findByIdAndUserId(Integer id, Integer userId);
 
     Page<Booking> findAll(Pageable pageable);

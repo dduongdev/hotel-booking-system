4640d27b85053e95f548d9b9ec18d67a2646986e
dduongdev <nguyendongduong4551190010@gmail.com>
2026-06-23 09:59:59 +0700

    Rmoved unused libs

--- src/main/java/com/dduongdev/hotel/entity/OtpVerification.java
+++ src/main/java/com/dduongdev/hotel/entity/OtpVerification.java
@@ -2,13 +2,8 @@ package com.dduongdev.hotel.entity;
 
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.CreationTimestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
